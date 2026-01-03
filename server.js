a30b93481c004aa83dc73fa503c742d5e9408b7b
AtulRokade89 <atulro@gmail.com>
2026-01-03 16:25:47 +0530

    fix: use Railway DATABASE_URL for postgres

--- server.js
+++ server.js
@@ -28,11 +28,7 @@ const userRoutes = require('./routes/user.js');
 
 // --- DB Pool ---
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
